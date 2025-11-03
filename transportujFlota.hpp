71fb969e379a512908f0b466e57dd1b77a17fa21
Jakub Orlowski <orlowski.jakub9@gmail.com>
2025-11-03 14:26:43 +0100

    z3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,21 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
-}
+    if (towar == 0) {
+        return 0;
+    }
+    Stocznia stocznia;
+     int  licznik        = 0;
+      int total_zaglowce = 0;
+     
+
+        while (licznik < towar) {
+            Statek* s2 = stocznia();
+            licznik += s2->transportuj();
+            if (dynamic_cast<Zaglowiec*>(s2) != nullptr) {
+                total_zaglowce ++;
+            }
+            delete s2;
+        }
+        return total_zaglowce;
+    }
